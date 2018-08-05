b703390fe4ef529b16f62a4cd1d00af12ed323a7
Matt Menzenski <matt@mobilgov.com>
2018-08-05 13:26:44 -0500

    couple new dependencies

--- project.clj
+++ project.clj
@@ -3,4 +3,7 @@
   :url "https://github.com/menzenski/encountr"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]])
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [com.cemerick/url "0.1.1"]
+                 [clj-http "3.9.1"]
+                 [cheshire "5.8.0"]])
