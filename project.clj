1b5b25d5ef6f341482c77c1a110a30ff544cc553
Matt Menzenski <matt@mobilgov.com>
2018-08-05 13:19:12 -0500

    and in project

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 (defproject encountr "0.1.0-SNAPSHOT"
-  :description "A wrapper for the 5E API ( https://fivee-api.com )"
+  :description "A wrapper for the D&D 5E API ( http://www.dnd5eapi.co/ )"
   :url "https://github.com/menzenski/encountr"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
