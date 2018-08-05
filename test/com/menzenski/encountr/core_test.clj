30f586c97d559d76b8d9f4ffe622032e214d8878
Matt Menzenski <matt@mobilgov.com>
2018-08-05 12:58:54 -0500

    unchange namespaces

--- test/com/menzenski/encountr/core_test.clj
+++
@@ -1,9 +0,0 @@
-(ns com.menzenski.encountr.core-test
-  (:require [clojure.test :refer :all]
-            [com.menzenski.encountr.core :as encountr-core]))
-
-(deftest a-test
-  (testing "stub test"
-    (is (= 1 1))))
-
-
