d66637589d669cee7641547d7ec5c1b64a0e0694
Matt Menzenski <matt@mobilgov.com>
2018-08-05 22:55:18 -0500

    add deploy information

--- project.clj
+++ project.clj
@@ -7,4 +7,6 @@
                  [clj-http "3.9.1"]
                  [com.cemerick/url "0.1.1"]
                  [cheshire "5.8.0"]
-                 [com.gfredericks/vcr-clj "0.4.16"]])
+                 [com.gfredericks/vcr-clj "0.4.16"]]
+  :deploy-repositories [["releases" :clojars]
+                        ["snapshots" :clojars]])
